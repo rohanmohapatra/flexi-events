3a751196a2a1f31347153451180aba4d5e6ee862
Ho <kenho12251999@gmail.com>
2023-04-13 10:22:46 -0700

    correction to boilerplate code

--- code/flexi-events/src/App.js
+++ code/flexi-events/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 
 import React from 'react';
 import HomePage from './pages/Home';
+import AttendeesPage from './pages/Attendees';
+import OrganizersPage from './pages/Organizers';
 
 function App() {
   return (
