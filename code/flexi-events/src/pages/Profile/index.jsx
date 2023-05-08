6aeac0d08931bbc0a3416c32772ac04038b56f08
mmixez <mmmixez@gmail.com>
2023-05-07 20:08:45 -0700

    profile page completed

--- code/flexi-events/src/pages/Profile/index.jsx
+++ code/flexi-events/src/pages/Profile/index.jsx
@@ -49,37 +49,56 @@ import SignedInLayout from "components/SignedInLayout";
           color="white"
           spacing={7}
         >
+        
+        <Typography sx={{ fontSize:24, color:"#ccccff", mt:2 }} variant="h5" component="h2">
+            User PROFILE 
+            </Typography>
 
-        <Typography variant="h5" component="h2">
+        <Typography sx={{ fontSize:22, color:"#624cab" }} variant="h5" component="h2">
              {user.email}
-          </Typography>
-
-          <Typography variant="h5" component="h2">
-            YOUR PROFILE
+         
+        
           </Typography>
           <Stack width="40%" alignItems="center" spacing={5}>
             <Avatar sx={{ width: "7rem", height: "7rem" }}></Avatar>
             
           </Stack>
 
-          <Typography sx={{ fontSize:20, color:"red" }}variant="h5" component="h2">
-            Name {" :"}
-          <Typography sx={{ fontSize:15, color:"white" }}variant="h5" component="span">
+          <Typography sx={{ fontSize:22, color:"#ccccff" }}variant="h5" component="h2">
+            Name {" : "}
+          <Typography sx={{ fontSize:22, color:"#fff0db" }}variant="h5" component="span">
             {user.name}
           </Typography>
           </Typography>
-          <Typography variant="h5" component="h2">
-            Email  {user.email}
+
+          <Typography sx={{ fontSize:22, color:"#ccccff" }} variant="h5" component="h2">
+          Email {" : "}
+          <Typography sx={{ fontSize:22, color:"#fff0db" }} variant="h5" component="span">
+             {user.email}
           </Typography>
-          <Typography variant="h5" component="h2">
+         </Typography>
+
+         <Typography sx={{ fontSize:22, color:"#ccccff" }} variant="h5" component="h2">
+          Phone Number {" : "}
+          <Typography sx={{ fontSize:22, color:"#fff0db" }} variant="h5" component="span">
           {user.phoneNumber}
           </Typography>
-          <Typography variant="h5" component="h2">
-            Role {user.role}
           </Typography>
-          <Typography variant="h5" component="h2">
+         
+          <Typography sx={{ fontSize:22, color:"#ccccff" }} variant="h5" component="h2">
+          Role {" : "}
+          <Typography sx={{ fontSize:22, color:"#fff0db" }} variant="h5" component="span">
+          {user.role}
+          </Typography>
+          </Typography>
+
+          <Typography sx={{ fontSize:22, color:"#ccccff" }} variant="h5" component="h2">
+          Pronouns {" : "}
+          <Typography sx={{ fontSize:22, color:"#fff0db" }} variant="h5" component="span">
             Pronouns {user.pronouns}
           </Typography>
+          </Typography>
+
         </Stack>
       </SignedInLayout>
     );
